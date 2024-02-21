e8687e87c00e913908935b63a077ae422ff07668
Martin Flischman <hem0inn@icloud.com>
2024-02-21 16:38:09 +0200

    Initial commit
    feat: added initial HTML, CSS, and JavaScript files to kickstart the project
    
    feat: included basic structure and styling along with initial JavaScript code

---
+++ index.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-database.js";
+
+// Firebase configuration
+const firebaseConfig = {
+    databaseURL: process.env.LUNCH_VOTE_DB
+};
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const placesRef = ref(database, "places");
+
+// Page Elements
+const placeForm = document.getElementById('placeForm');
+const placesList = document.getElementById('placesList');
+const placeInput = document.getElementById('placeInput');
+const leadingPlaceDisplay = document.getElementById('leadingPlace');
